238a91a2526af37f618c29745f1017aff8160ee8
GutsanDVC <bastian.gutierrez@dvc.cl>
2025-10-14 11:29:10 -0300

    Fix: se modifica el formato del log para encontrar de mejor forma los errores

--- portal_finiquitos/settings.py
+++ portal_finiquitos/settings.py
@@ -226,11 +226,18 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {message}",
+            "style": "{",
+        },
+    },
     "handlers": {
         "file": {
             "level": "DEBUG",
             "class": "logging.FileHandler",
             "filename": BASE_DIR / "django_debug.log",
+            "formatter": "verbose",
         },
     },
     "loggers": {
